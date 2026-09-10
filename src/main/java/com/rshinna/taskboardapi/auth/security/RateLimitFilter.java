608da0c119280ce6336d6d4a8cf22c341db5aec0
Rodrigo Franco <rshina@hotmail.com>
2026-09-10 20:32:23 -0300

    chore: remove unused imports from RateLimitFilter and GlobalExceptionHandlerTest

--- src/main/java/com/rshinna/taskboardapi/auth/security/RateLimitFilter.java
+++ src/main/java/com/rshinna/taskboardapi/auth/security/RateLimitFilter.java
@@ -14,7 +14,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 @Slf4j
